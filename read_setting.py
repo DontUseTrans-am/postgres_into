c0695dbafc064d8a2d6905427d3f0d0843483512
yuki <y-horita-42t@eagle.sophia.ac.jp>
2018-10-31 03:08:05 +0900

    ファイル読み込み用プログラムを追加

---
+++ read_setting.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class Connection(object):
+    def __init__(self,filename="settings.csv"):
+def (file_name):
+    df=pd.read_csv(file_name,header=None,idnex_col=0)
+
+if __name__=="__main__":
+    
